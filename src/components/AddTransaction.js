54ff25cbf295fb60443a75fffb4c32b884858ceb
Tanish Panjwani <tanish@paywise.io>
2021-07-22 00:51:11 +0300

    first commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,56 @@
+import React, { useState, useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState'
+
+
+const AddTransaction = () => {
+    const [text, setText] = useState("")
+    const [amount, setAmount] = useState(0)
+    const  { addTransaction } = useContext(GlobalContext)
+
+
+    const onSubmit = e => {
+        e.preventDefault()
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text, 
+            amount: +amount
+        } 
+        addTransaction(newTransaction)
+    }
+
+
+    return (
+        <>
+        <h3>Add new transaction</h3>
+        <form id="form" onSubmit={onSubmit}>
+            <div className="form-control">
+            <label htmlFor="text">Text</label>
+            <input 
+                type="text" 
+                id="text" 
+                placeholder="Enter text..." 
+                onChange={(e)=> setText(e.target.value)}
+                value={text}
+                />
+            </div>
+            <div className="form-control">
+            <label htmlFor="amount">
+                Amount 
+                <br />
+                (negative - expense, positive - income)
+            </label>
+            <input 
+                type="number" 
+                id="amount" 
+                placeholder="Enter amount..." 
+                onChange={(e)=> setAmount(e.target.value)}
+                value={amount}
+                />
+            </div>
+            <button className="btn">Add transaction</button>
+        </form>
+        </>
+    )
+}
+
+export default AddTransaction
---
+++ src/components/Transaction.js
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState'
+
+
+const Transaction = ({transaction}) => {
+  const { deleteTransaction } = useContext(GlobalContext)
+
+    const sign = transaction.amount < 0? '-': '+'
+    return (
+        <li class={sign === '-'? 'minus': 'plus'}>
+          {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+          <button onClick = {()=> deleteTransaction(transaction.id)}class="delete-btn">x</button>
+        </li> 
+    )
+}
+
+export default Transaction
